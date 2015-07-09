d568d2f187592951e81b3fbe878577da10208976
apoorv-vijay-joshi <avjoshi@ncsu.edu>
2015-07-09 12:46:41 -0400

    Update insightDataScience.py

--- insightDataScience.py
+++ insightDataScience.py
@@ -25,10 +25,11 @@ for line in my_list:            # Accesses each tweet at a time.
     uniqueWords.sort()
     # median is the integer variable to store the latest median value.
     if len(uniqueWords)%2==0:
-        median = (uniqueWords[int(len(uniqueWords)/2)]+uniqueWords[int((len(uniqueWords)/2))-1])/2
+        median = float((uniqueWords[int(len(uniqueWords)/2)]+uniqueWords[int((len(uniqueWords)/2))-1])/2)
     else:
-        median = uniqueWords[int(((len(uniqueWords)+1)/2))-1]
+        median = float(uniqueWords[int(((len(uniqueWords)+1)/2))-1])
     # Write the latest modified median value to the 'ft2.txt' file.
+    median=round(median,2)
     tweetsMedian.writelines(format(median)+'\n')
     for element in words:       # Accesses each word in the list of words in the list
         if element in wordDict.keys():
@@ -36,7 +37,7 @@ for line in my_list:            # Accesses each tweet at a time.
         else:
             tempDict={element : 1}
             wordDict.update(tempDict)   # if we come across a new word, we add the word to the dictionary with value 1.
-    words.clear()
+    del words[:]
 # Open/Create the file 'ft1.txt' in the 'tweets_output' directory to write in to.
 tweetsUniqueWordCount = open("tweets_output/ft1.txt", "w")
 for keys,values in wordDict.items():
